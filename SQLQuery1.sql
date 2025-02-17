94cd9707c8ef8881454c6b0c20205abe92a114c6
stevygmillenium <stevyg2008@hotmail.com>
2025-02-17 16:11:48 -0500

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[per_Table] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [per_name]   VARCHAR (MAX) NULL,
+    [email]       VARCHAR (150) NULL,
+    [phone]       VARCHAR (50)  NULL,
+    [d_o_b]  DATETIME      NULL,
+    [addresses_info] XML           NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
